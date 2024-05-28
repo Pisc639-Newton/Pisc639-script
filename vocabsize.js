27ef4dfdacdae070f9e71248ae306b635c1eadc3
Pisc639-Newton <161461821+Pisc639-Newton@users.noreply.github.com>
2024-05-28 12:28:54 +0700

    Update vocabsize.js

--- vocabsize.js
+++ vocabsize.js
@@ -68,7 +68,7 @@
     $(".footer .container-fluid .row .col-6 .btn_save").click();
     $('#btn_skip button').click();
     //try {$('.desktop-screen div').filter(function() {var style = $(this).attr('style');return style === "background: linear-gradient(to top, #679ABF 99%, #6D6E71 1%);border-radius:15px;" ||style === "background: linear-gradient(to bottom, #6D6E71 100%, #679ABF 0% );border-radius:15px;";})[0].click()} catch {}
-    for (var percentage = 1; percentage <= 100; percentage += 0.01) {try {var divs = $('.desktop-screen div').filter(function() {var style = $(this).attr('style');return style === "background: linear-gradient(to top, #679ABF, #6D6E71 " + percentage + "%);border-radius:15px;"|| style === "background: linear-gradient(to bottom, #6D6E71 100%, #679ABF " + percentage + "% );border-radius:15px;";});if (divs.length > 0) {divs[0].click();break;}} catch (error) {console.error(error);}}
+    $('.desktop-screen div').each(function() { var style = $(this).attr('style'), percentage = style && (style.includes("linear-gradient(to top") ? style.split("linear-gradient(to top, #679ABF, #6D6E71 ")[1].split("%")[0] : style.includes("linear-gradient(to bottom") ? style.split("linear-gradient(to bottom, #6D6E71 100%, #679ABF ")[1].split("%")[0] : null); if (percentage) { $(this)[0].click(); return false; } });
     try {$('img').filter(function() {var src = $(this).attr('src');return src === "https://vocabsize.xeersoft.co.th/assets/images/my_vocab_assign.png";})[0].click()} catch {}
     delaym(2000);
     if (window.location.pathname.includes('basic') || window.location.pathname.includes('practice') || window.location.pathname.includes('checklist')) {

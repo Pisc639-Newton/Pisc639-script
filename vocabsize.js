e6f5d97a75e103d29b48a1bf5c883efb70a18b6e
Pisc639-Newton <161461821+Pisc639-Newton@users.noreply.github.com>
2024-05-28 12:13:48 +0700

    Update vocabsize.js

--- vocabsize.js
+++ vocabsize.js
@@ -71,11 +71,15 @@
     try {$('img').filter(function() {var src = $(this).attr('src');return src === "https://vocabsize.xeersoft.co.th/assets/images/my_vocab_assign.png";})[0].click()} catch {}
     delaym(2000);
     if (window.location.pathname.includes('basic') || window.location.pathname.includes('practice') || window.location.pathname.includes('checklist')) {
-        if ('referrer' in document) {
-            delaym(500)
-            window.location = document.referrer;
+        if (window.location.pathname.includes('checklist')) {
+            window.location = "https://vocabsize.xeersoft.co.th/dashboard";
         } else {
-            window.history.back();
+            if ('referrer' in document) {
+                delaym(500)
+                window.location.pathname = document.referrer;
+            } else {
+                window.history.back();
+            }
         }
     }
 })();

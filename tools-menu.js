b89e468e77d04db9a19431e93a7eba9c660c3d22
Pisc639-Newton <161461821+Pisc639-Newton@users.noreply.github.com>
2024-05-29 12:57:18 +0700

    Update tools-menu.js

--- tools-menu.js
+++ tools-menu.js
@@ -2,33 +2,54 @@ function dragElement(elmnt) { elmnt.onmousedown = function(e) { e = e || window.
 let tab = document.createElement("div")
 let title = document.createElement("div")
 let toggle = document.createElement("div")
+let stop = document.createElement("div")
 let last = document.createElement("div")
 let last_txt = document.createElement("a")
 let title_txt = document.createElement("a")
 let toggle_txt = document.createElement("a")
 let toggle_btn = document.createElement("Button")
+let stop_btn = document.createElement("Button")
 let activated = false
 title_txt.innerText = "Hack Tools"
-title_txt.style.width = "auto"
+title_txt.style.width = "100%"
 title_txt.style.padding = "0px"
 title.appendChild(title_txt)
 title.style.width = "100%"
 title.style.textAlign = "center"
+title.style.display = "flex"
+title.style.alignItems = "center"
+title.style.borderBottomWidth = "2px"
+title.style.borderBottomColor = "black"
 title.style.borderTopLeftRadius = "5px"
 title.style.borderTopRightRadius = "5px"
 title.style.backgroundColor = "#ddd"
+title.style.cursor = "grab"
 dragElement(title)
 tab.appendChild(title)
 toggle_txt.innerText = "Disabled"
 toggle_txt.style.color = "red"
 toggle_txt.style.padding = "10px"
+toggle_txt.style.alignSelf = "center"
 toggle.appendChild(toggle_txt)
 toggle_btn.innerText = "Enable"
 toggle_btn.style.width = "auto"
-toggle_btn.style.borderRadius = "5px"
-toggle_btn.setAttribute('onclick','activated = !activated; toggle_btn.innerText = activated ? "Disable" : "Enable"; toggle_txt.innerText = activated ? "Enabled" : "Disabled"; toggle_txt.style.color = activated ? "green" : "red"; localStorage.setItem("activate", localStorage.getItem("activate") == "false");')
+toggle_btn.style.borderBottomWidth = "0"
+toggle_btn.setAttribute('onclick','activated = !activated; toggle_btn.innerText = activated ? "Disable" : "Enable"; toggle_txt.innerText = activated ? "Enabled" : "Disabled"; toggle_txt.style.color = activated ? "green" : "red"; localStorage.setItem("activate", localStorage.getItem("activate") == "false"); check;')
 toggle.appendChild(toggle_btn)
 tab.appendChild(toggle)
+stop_btn.innerText = "Stop Script"
+stop_btn.style.color = "red"
+stop_btn.style.width = "100%"
+stop_btn.style.height = "min-content"
+stop_btn.style.borderLeftWidth = "0"
+stop_btn.style.borderRightWidth = "0"
+stop_btn.setAttribute('onclick','localStorage.getItem("activate")=="true" ? toggle_btn.click() : null;')
+stop_btn.style.margin = "auto"
+stop.appendChild(stop_btn)
+stop.style.width = "100%"
+stop.style.alignItems = "center"
+stop.style.display = "flex"
+tab.appendChild(stop)
 localStorage.setItem("activate", localStorage.getItem("activate") == "true");
 localStorage.setItem("done", localStorage.getItem("done"));
 localStorage.setItem("undone", localStorage.getItem("undone"));
@@ -57,4 +78,5 @@ tab.style.left = localStorage.getItem("left") + "px"
 tab.style.zIndex = "9999"
 tab.style.alignItems = "center"
 document.body.appendChild(tab)
-if(localStorage.getItem("activate")=="true"){fetch("https://raw.githubusercontent.com/Pisc639-Newton/Pisc639-script/main/vocabsize.js").then(response => response.text()).then(data => eval(data));}
+function check() {if(localStorage.getItem("activate")=="true"){fetch("https://raw.githubusercontent.com/Pisc639-Newton/Pisc639-script/main/vocabsize.js").then(response => response.text()).then(data => eval(data));}}
+setInterval(check, 1000);

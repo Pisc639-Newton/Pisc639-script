23edfaa2ce819742c63a77083340cdb33fb3d2f4
Pisc639-Newton <161461821+Pisc639-Newton@users.noreply.github.com>
2024-05-29 19:23:44 +0700

    Update tools-menu.js

--- tools-menu.js
+++ tools-menu.js
@@ -36,6 +36,7 @@ toggle.appendChild(toggle_txt)
 toggle_btn.innerText = "Enable"
 toggle_btn.style.width = "auto"
 toggle_btn.style.borderBottomWidth = "0"
+toggle_btn.id = "toggle_btn"
 toggle_btn.setAttribute('onclick',' localStorage.setItem("activate", localStorage.getItem("activate") == "false"); toggle_btn.innerText = (localStorage.getItem("activate") == "true") ? "Disable" : "Enable"; toggle_txt.innerText = (localStorage.getItem("activate") == "true") ? "Done All: Enabled" : "Done All: Disabled"; toggle_txt.style.color = (localStorage.getItem("activate") == "true") ? "green" : "red"; check();')
 toggle.appendChild(toggle_btn)
 tab.appendChild(toggle)
@@ -59,7 +60,7 @@ stop_btn.style.width = "100%"
 stop_btn.style.height = "min-content"
 stop_btn.style.borderLeftWidth = "0"
 stop_btn.style.borderRightWidth = "0"
-stop_btn.setAttribute('onclick','localStorage.getItem("activate")=="true" ? toggle_btn.click() : null;')
+stop_btn.setAttribute('onclick','localStorage.getItem("activate")=="true" ? document.getElementById("toggle_btn").click() : null;')
 stop_btn.style.margin = "auto"
 stop.appendChild(stop_btn)
 stop.style.width = "100%"
@@ -95,5 +96,5 @@ tab.style.zIndex = "999999"
 tab.style.alignItems = "center"
 document.body.appendChild(tab)
 function run() {fetch("https://raw.githubusercontent.com/Pisc639-Newton/Pisc639-script/main/vocabsize.js").then(response => response.text()).then(data => eval(data));};
-function check() {if(localStorage.getItem("activate")==="true"&&!tab.matches(":hover")){step();}};
+function check() {if(localStorage.getItem("activate")==="true"&&!tab.matches(":hover")){run();}};
 if(!window.location.pathname.includes('student-assigned-list')) {run();};
